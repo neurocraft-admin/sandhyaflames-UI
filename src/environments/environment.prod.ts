a92a9f158f673af4c4eefaf5f78cf7fa4ff57b05
cloudguruadmin <admin@mthdcc.com>
2025-10-26 15:56:48 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.14.178.225:5027/api'  // ✅ use your actual GCP API endpoint
+  apiUrl: 'http://34.14.178.225:5027/api'  // ✅ use your actual GCP API endpoint corrected
 };
