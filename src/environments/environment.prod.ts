2ab09f59c444f66bbf83aa407a9f593438727847
cloudguruadmin <admin@mthdcc.com>
2026-04-25 13:44:01 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: '/api',
+//   emailDomain: 'sandhyaflames.in'
+// };
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://api.flamemitra.in',  // ← Cloud Run URL
   emailDomain: 'sandhyaflames.in'
 };
