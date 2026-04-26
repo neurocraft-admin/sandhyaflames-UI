314a8c0b098a841a4afa83b78cdb5b5355ce68b2
cloudguruadmin <admin@mthdcc.com>
2026-04-26 07:40:36 +0530

    env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+// environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://api.flamemitra.in',
+  apiUrl: 'https://api.flamemitra.in/api',  // ✅ added /api
   emailDomain: 'sandhyaflames.in'
 };
