5d4e9337b017a7f76589fb06629122ecf7e2a8b8
neurocraft-admin <neurocraftinfo@gmail.com>
2026-06-06 20:07:15 +0530

    Point Angular to Hostinger API for Testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://api.flamemitra.in/api',  // ✅ added /api
+  apiUrl: 'http://187.127.173.240/api',
   emailDomain: 'sandhyaflames.in'
 };
