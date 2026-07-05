74e4541c6e6772c1f271f61992dc966c0b9e3995
neurocraft-admin <neurocraftinfo@gmail.com>
2026-07-05 12:28:58 +0530

    Update apiUrl to new endpoint for QA setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-// environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://api.flamemitra.in/api',
+  apiUrl: 'http://onkzihiks3ss0hadngclcsyi.187.127.173.240.sslip.io/api',
   emailDomain: 'sandhyaflames.in'
 };
