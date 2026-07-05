409e3b373a1e3c8d00852cbe5ecd715d82a05434
neurocraft-admin <neurocraftinfo@gmail.com>
2026-07-05 13:11:59 +0530

    Update API URL for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://onkzihiks3ss0hadngclcsyi.187.127.173.240.sslip.io/api',
+  apiUrl: 'https://qa-api.flamemitra.in/api',
   emailDomain: 'sandhyaflames.in'
 };
